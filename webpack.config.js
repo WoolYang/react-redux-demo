2b95b3e4547d1cc57824e625af47b9dcef538daa
woolyang <249877448@qq.com>
2017-06-01 19:41:02 +0800

    提交

--- webpack.config.js
+++ webpack.config.js
@@ -52,7 +52,7 @@ module.exports = {
     },
 
     devServer: {
-        // contentBase: './src/views'  //本地服务器所加载的页面所在的目录
+        //contentBase: './dist'  //本地服务器所加载的页面所在的目录
         port: 8888,
         colors: true,  //终端中输出结果为彩色
         historyApiFallback: true,  //不跳转
@@ -60,9 +60,7 @@ module.exports = {
     },
 
     plugins: [
-      //  new webpack.NoErrorsPlugin(),
-      // new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /zh-cn/),
-      new ExtractTextPlugin('main.css')
+       new ExtractTextPlugin('main.css')
     ]
 
 }
